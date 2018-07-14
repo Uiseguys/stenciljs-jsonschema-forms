53486706a4bac4cb1e1fe7f9ba98efae35946595
gafilipoie <marius.afilipoie@gmail.com>
2018-07-14 23:30:00 +0300

    small fix

--- stencil.config.js
+++ stencil.config.js
@@ -2,7 +2,7 @@ const sass = require('@stencil/sass');
 
 exports.config = {
     namespace: 'formGenerator',
-    // generateDistribution: true,
+    generateDistribution: true,
     // serviceWorker: true,
     outputTargets: [{ type: 'www' }, { type: 'dist' }],
     plugins: [sass()]
