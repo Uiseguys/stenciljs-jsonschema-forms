8fb144b4081852d6097eed95a31f9859ed32a9a9
gafilipoie <marius.afilipoie@gmail.com>
2018-07-15 00:49:40 +0300

    trying to make it work

--- stencil.config.js
+++ stencil.config.js
@@ -1,7 +1,7 @@
 const sass = require('@stencil/sass');
 
 exports.config = {
-    namespace: 'formGenerator',
+    namespace: 'index',
     // generateDistribution: true,
     // serviceWorker: true,
     outputTargets: [{ type: 'www' }, { type: 'dist' }],
