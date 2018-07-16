cbcca0469be2ba264e8eeb06a4818c045c2a086f
gafilipoie <marius.afilipoie@gmail.com>
2018-07-16 19:27:13 +0300

    changed settings

--- stencil.config.js
+++ stencil.config.js
@@ -1,7 +1,7 @@
 const sass = require('@stencil/sass');
 
 exports.config = {
-    namespace: 'formGenerator',
+    namespace: 'form-generator',
     // generateDistribution: true,
     // serviceWorker: true,
     outputTargets: [{ type: 'www' }, { type: 'dist' }],
